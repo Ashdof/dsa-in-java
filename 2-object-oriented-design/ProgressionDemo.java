7141a197bbb114a93a162f1aec2e75219d99b055
Emmanuel Enchill <mzigah4@gmail.com>
2025-10-09 08:01:45 +0000

    Created main class to test progression class

---
+++ 2-object-oriented-design/ProgressionDemo.java
@@ -0,0 +1,11 @@
+import progression.Progression;
+
+public class ProgressionDemo {
+    public static void main(String[] args) {
+	Progression progression;
+	int endValue = 20;
+
+	progression = new Progression();
+	progression.printProgression(endValue);
+    }
+}
