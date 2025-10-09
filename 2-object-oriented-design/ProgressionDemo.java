9dcba773d0b0a11f13721f37dbeef16add17ca23
Emmanuel Enchill <mzigah4@gmail.com>
2025-10-09 08:27:53 +0000

    Modified main class to test the arithmetic progression subclass

--- 2-object-oriented-design/ProgressionDemo.java
+++ 2-object-oriented-design/ProgressionDemo.java
@@ -1,11 +1,18 @@
-import progression.Progression;
+import progression.*;
 
 public class ProgressionDemo {
     public static void main(String[] args) {
+	
 	Progression progression;
-	int endValue = 20;
+	ArithmeticProgression arithmeticProgression;
+	int endValue = 20, step = 4;
 
 	progression = new Progression();
+	System.out.print("Normal progression: ");
 	progression.printProgression(endValue);
+
+	arithmeticProgression = new ArithmeticProgression(step);
+	System.out.print("Arithmetic progression: ");
+	arithmeticProgression.printProgression(endValue);
     }
 }
