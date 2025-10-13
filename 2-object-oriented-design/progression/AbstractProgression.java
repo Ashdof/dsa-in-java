71515c2b5779d46ab01e914bc5aaa5cdde7f11d1
Emmanuel Enchill <mzigah4@gmail.com>
2025-10-13 08:23:49 +0000

    Created an abstract class. This cover lesson on abstract class and abstract method

---
+++ 2-object-oriented-design/progression/AbstractProgression.java
@@ -0,0 +1,33 @@
+package progression;
+
+/**
+ * Definition of abstract base class
+ */
+public abstract class AbstractProgression {
+
+    protected long current;
+
+    public AbstractProgression(long start) {
+
+	this.current = start;
+    }
+
+    public AbstractProgression() {
+	this(0);
+    }
+
+    public long nextValue() {       // this is a concrete method
+	long answer = current;
+	advance();   // this protected call is responsible for advancing the current value
+	return answer;
+    }
+
+    public void printProgression(int n) {   // this is a concrete method
+	System.out.println(nextValue());  // print first value without leading space
+	for (int j = 1; j < n; j++)
+	    System.out.println(" " + nextValue()); // print leading space before others
+	System.out.println();  // end the line
+    }
+
+    protected abstract void advance();  // abstract method without method body
+}
